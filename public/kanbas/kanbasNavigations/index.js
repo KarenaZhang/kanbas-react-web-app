41f05f0ae4c79c02571fcd4833ecbcbed518025c
kekez <zhang.kek@northeastern.edu>
2023-10-04 23:46:24 -0400

    assignment 2

---
+++ public/kanbas/kanbasNavigations/index.js
@@ -0,0 +1,11 @@
+function kanbasNavigation() {
+    return `
+<ul>
+    <li><a href="/kanbas/Account/Profile/screen.html"></li>
+    <li><a href="/kanbas/Dashboard/screen.html">Dashboard</li>
+    <li><a href="/kanbas/Courses/Home/screen.html"></li>
+</ul>
+`;
+}
+
+export default kanbasNavigation;
