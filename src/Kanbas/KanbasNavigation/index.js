40b3be19e4cc2ed3b315c1ac71464935f185835e
kekez <zhang.kek@northeastern.edu>
2023-12-04 15:59:37 -0600

    a6

--- src/Kanbas/KanbasNavigation/index.js
+++ src/Kanbas/KanbasNavigation/index.js
@@ -1,14 +1,57 @@
+// import { Link, useLocation } from "react-router-dom";
+//
+// function KanbasNavigation() {
+//     const links = ["Account", "Dashboard", "Courses", "Calendar", "Signin", "Signup", "Account2", "admin/users"];
+//
+//
+//     const { pathname } = useLocation();
+//     return (
+//         <div className="list-group" style={{ width: 150 }}>
+//             {links.map((link, index) => (
+//                 <Link
+//                     key={index}
+//                     to={`/Kanbas/${link}`}
+//                     className={`list-group-item ${pathname.includes(link) && "active"}`}>
+//                     {link}
+//                 </Link>
+//             ))}
+//         </div>
+//     );
+// }
+// export default KanbasNavigation;
+
 import { Link, useLocation } from "react-router-dom";
+import { BiUserCircle } from "react-icons/bi";
+import { RiDashboard3Fill } from "react-icons/ri";
+import { FaBook, FaInbox, FaHistory, FaCameraRetro, FaShareSquare, FaQuestionCircle } from "react-icons/fa";
+import { BsFillCalendar2WeekFill } from "react-icons/bs";
+import "../../index.css"
 function KanbasNavigation() {
-    const links = ["Account", "Dashboard", "Courses", "Calendar"];
+    const links = ["Account", "Dashboard", "Courses", "Calendar", "Inbox", "History", "Studio", "Commons", "Help", "Signin", "Signup", "Account2", "admin/users"];
+    const icons = {
+        Dashboard: <RiDashboard3Fill className="wd-icon"/>,
+        Courses: <FaBook className="wd-icon"/>,
+        Calendar: <BsFillCalendar2WeekFill className="wd-icon" />,
+        Inbox: <FaInbox className="wd-icon" />,
+        History: <FaHistory className="wd-icon" />,
+        Studio: <FaCameraRetro className="wd-icon" />,
+        Commons: <FaShareSquare className="wd-icon" />,
+        Account2: <BiUserCircle className="wd-icon wd-user-icon" />,
+        Signin: <BiUserCircle className="wd-icon wd-user-icon" />,
+        Signup: <BiUserCircle className="wd-icon wd-user-icon" />,
+        Account: <BiUserCircle className="wd-icon wd-user-icon" />,
+    }
+
     const { pathname } = useLocation();
     return (
-        <div className="list-group" style={{ width: 150 }}>
+        <div className="list-group wd-kanbas-navigation" style={{ width: 150 }}>
             {links.map((link, index) => (
                 <Link
                     key={index}
                     to={`/Kanbas/${link}`}
                     className={`list-group-item ${pathname.includes(link) && "active"}`}>
+
+                    {icons[link]}<br />
                     {link}
                 </Link>
             ))}
@@ -16,3 +59,4 @@ function KanbasNavigation() {
     );
 }
 export default KanbasNavigation;
+
--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,13 +1,102 @@
-import KanbasNavigation from "./KanbasNavigation";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Dashboard from "./Dashboard";
-import Courses from "./Courses";
-import db from "./Database";
-import { React, useState, useEffect } from "react";
-import store from "./store";
-import { Provider } from "react-redux";
-import axios from "axios";
+// import KanbasNavigation from "./KanbasNavigation";
+// import { Routes, Route, Navigate } from "react-router-dom";
+// import Dashboard from "./Dashboard";
+// import Courses from "./Courses";
+// import db from "./Database";
+// import { React, useState, useEffect } from "react";
+// import store from "./store";
+// import { Provider } from "react-redux";
+// import axios from "axios";
+// import Account2 from "./users/account";
+// import Signin from "./users/signin";
+// import Signup from "./users/signup";
+// import UserTable from "./users/table";
+//
+// function Kanbas() {
+//     const [courses, setCourses] = useState([]);
+//     const API_BASE = process.env.REACT_APP_API_BASE;
+//     const URL = `${API_BASE}/api/courses`;
+//     const findAllCourses = async () => {
+//         const response = await axios.get(URL);
+//         setCourses(response.data);
+//     };
+//     useEffect(() => {
+//         findAllCourses();
+//     }, []);
+//
+//     const [course, setCourse] = useState({
+//         name: "New Course",      number: "New Number",
+//         startDate: "2023-09-06", endDate: "2023-12-23",
+//     });
+//     const addNewCourse = async () => {
+//         const response = await axios.post(URL, course);
+//         setCourses([response.data, ...courses,]);
+//     };
+//     const deleteCourse = async (course) => {
+//         const response = await axios.delete(`${URL}/${course._id}`);
+//         setCourses(courses.filter((c) => c._id !== course._id));
+//     };
+//     const updateCourse = async (course) => {
+//         const response = await axios.put(`${URL}/${course._id}`, course);
+//         setCourses(
+//             courses.map((c) => {
+//                 if (c._id === course._id) {
+//                     return response.data;}
+//                 return c;
+//             })
+//         );
+//         setCourse({ name: "" });
+//     };
+//     return (
+//         <Provider store={store}>
+//             <div className="d-flex">
+//                 <KanbasNavigation />
+//                 <div>
+//                     <Routes>
+//                         <Route path="/" element={<Navigate to="Dashboard" />} />
+//                         <Route path="Signin" element={<Signin />} />
+//                         <Route path="Signup" element={<Signup />} />
+//                         <Route path="Account" element={<h1>Account</h1>} />
+//                         <Route path="Dashboard" element={<Dashboard
+//                                 courses={courses}
+//                                 course={course}
+//                                 setCourse={setCourse}
+//                                 addNewCourse={addNewCourse}
+//                                 deleteCourse={deleteCourse}
+//                                 updateCourse={updateCourse}/>
+//                         } ></Route>
+//                         <Route path="Account2" element={<Account2 />}></Route>
+//                         <Route path="Account2/:id" element={<Account2 />} />
+//                         <Route path="admin/Users" element={<UserTable />}></Route>
+//                         <Route path="Courses/:courseId/*" element={<Courses
+//                             courses={courses} />}></Route>
+//                         <Route path="Calendar" element={<h1>Calendar</h1>}></Route>
+//                     </Routes>
+//                 </div>
+//              </div>
+//         </Provider>
+//     );
+// }
+// export default Kanbas;
 
+
+
+
+import KanbasNavigation from "./KanbasNavigation";
+import { React, useState, useEffect } from "react";
+import db from "../Kanbas/Database"
+import { Routes, Route, Navigate } from "react-router"
+import Courses from "./Courses";
+import Account from "./Account";
+import Account2 from "../Kanbas/users/account.js";
+import Signin from "../Kanbas/users/signin";
+import Signup from "../Kanbas/users/signup";
+import UserTable from "../Kanbas/users/table";
+import AccountEdit from "./Account/Edit";
+import Dashboard from "./Dashboard";
+import store from "./store";
+import axios from "axios";
+import { Provider } from "react-redux";
 function Kanbas() {
     const [courses, setCourses] = useState([]);
     const API_BASE = process.env.REACT_APP_API_BASE;
@@ -19,25 +108,34 @@ function Kanbas() {
     useEffect(() => {
         findAllCourses();
     }, []);
-
     const [course, setCourse] = useState({
-        name: "New Course",      number: "New Number",
-        startDate: "2023-09-06", endDate: "2023-12-23",
+        name: "New Course", number: "New Number",
+        startDate: "2023-09-10", endDate: "2023-12-15",
     });
     const addNewCourse = async () => {
         const response = await axios.post(URL, course);
-        setCourses([response.data, ...courses,]);
+        setCourses([
+            response.data,
+            ...courses,
+        ]);
     };
     const deleteCourse = async (course) => {
-        const response = await axios.delete(`${URL}/${course._id}`);
-        setCourses(courses.filter((c) => c._id !== course._id));
+        const response = await axios.delete(
+            `${URL}/${course._id}`
+        );
+        setCourses(courses.filter(
+            (c) => c._id !== course._id));
     };
     const updateCourse = async (course) => {
-        const response = await axios.put(`${URL}/${course._id}`, course);
+        const response = await axios.put(
+            `${URL}/${course._id}`,
+            course
+        );
         setCourses(
             courses.map((c) => {
                 if (c._id === course._id) {
-                    return response.data;}
+                    return response.data;
+                }
                 return c;
             })
         );
@@ -50,24 +148,28 @@ function Kanbas() {
                 <div>
                     <Routes>
                         <Route path="/" element={<Navigate to="Dashboard" />} />
-                        <Route path="Account" element={<h1>Account</h1>} />
+                        <Route path="Signin" element={<Signin />} />
+                        <Route path="Signup" element={<Signup  />} />
+                        <Route path="Account" element={<Account />}></Route>
+                        <Route path="Account2" element={<Account2 />}></Route>
+                        <Route path="Account2/:id" element={<Account2 />} />
+                        <Route path="admin/Users" element={<UserTable />}></Route>
+                        <Route path="Account/Edit" element={<AccountEdit />}></Route>
                         <Route path="Dashboard" element={<Dashboard
-                                courses={courses}
-                                course={course}
-                                setCourse={setCourse}
-                                addNewCourse={addNewCourse}
-                                deleteCourse={deleteCourse}
-                                updateCourse={updateCourse}/>
-                        } ></Route>
+                            courses={courses}
+                            course={course}
+                            setCourse={setCourse}
+                            addNewCourse={addNewCourse}
+                            deleteCourse={deleteCourse}
+                            updateCourse={updateCourse} />
+                        }></Route>
                         <Route path="Courses/:courseId/*" element={<Courses
                             courses={courses} />}></Route>
                         <Route path="Calendar" element={<h1>Calendar</h1>}></Route>
                     </Routes>
                 </div>
-             </div>
+            </div>
         </Provider>
     );
 }
 export default Kanbas;
-
-
